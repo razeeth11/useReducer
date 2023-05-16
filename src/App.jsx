d8c6839a381b5dcf1197a3011fdfaa6210a235e5
razeeth11 <razeeth011@gmail.com>
2023-05-17 00:01:23 +0530

    Dispatch an action

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import "./App.css";
 import { useReducer } from "react";
 
-const initial_value = { count: 0 };
+const initial_value = { count: 0 , color : ""};
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "decrement":
-      return { count: state.count > 0 ? state.count - 1 : state.count };
+      return { color : state.color , count: state.count > 0 ? state.count - 1 : state.count };
     case "increment":
-      return { count: state.count + 1 };
+      return { color : state.color , count: state.count + 1 };
+    case 'reset':
+      return { color : state.color , count: action.payload };
+      case 'update color':
+        return { count : state.count , color : state.color = action.payload }
   }
 };
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initial_value);
+  const style = {
+    background : state.color
+  }
 
   return (
     <div className="App">
       <button onClick={() => dispatch({ type: "decrement" })}>-</button>
       count : {state.count}
       <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: 'reset' , payload : 0 })}>0</button>
+      <button onClick={() => dispatch({ type: 'reset' , payload : 100 })}>100</button>
+      <input style={style} type="text" onChange={(ev)=> dispatch({type : "update color" , payload : ev.target.value})}/>
     </div>
   );
 }
