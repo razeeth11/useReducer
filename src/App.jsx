fe6e5bd5b9060ab7e06b241a72f9e502589235ca
razeeth11 <razeeth011@gmail.com>
2023-05-16 22:10:29 +0530

    Dispatch an action

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css'
+import "./App.css";
+import { useReducer } from "react";
+
+const initial_value = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "decrement":
+      return { count: state.count - 1 };
+    case "increment":
+      return { count: state.count + 1 };
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initial_value);
 
   return (
-    <div className='App'>
-      
+    <div className="App">
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      count : {state.count}
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
